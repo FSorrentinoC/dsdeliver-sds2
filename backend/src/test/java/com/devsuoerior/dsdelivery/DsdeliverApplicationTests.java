e06083a85cde15e3b921e55a73d45ad421fc3f3f
FSorrentinoC <sorrentino@live.com>
2021-01-06 15:28:22 -0300

    Project created

---
+++ backend/src/test/java/com/devsuoerior/dsdelivery/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuoerior.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
